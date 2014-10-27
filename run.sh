fa099728ce5e30bec527ea5facaaa3687afd5b6c
Michael Kalantar <kalantar@us.ibm.com>
2014-10-27 14:07:52 -0400

    change in environment variable __STATUS__ --> __LOG__

--- run.sh
+++ run.sh
@@ -204,7 +204,7 @@ if [ ${DEBUG} -eq 0 ]; then
   # A new container was created and started, remove the previous one
   remove_previous
   # output resulting container
-  echo "{\"container\":\"${CONTAINER_ID}\"}" > $__STATUS__/out
+  echo "{\"container\":\"${CONTAINER_ID}\"}" > $__LOG__/out
     
 fi
 # cleanup request bodies
